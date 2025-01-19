a1ac20518a02484e40197f4078e0d3487a9de0ce
kpzip <kpzipgames@gmail.com>
2025-01-19 01:31:36 -0800

    work on deserializing stuff

--- Emulator/src/pic16instructionset.cpp
+++ Emulator/src/pic16instructionset.cpp
@@ -6,10 +6,23 @@
  */
 #include "pic16instructionset.h"
 
-static PIC16InstructionType PIC16Instruction::fromString(std::string_view str) const {
+PIC16InstructionType fromString(std::string_view str) {
 	// TODO
 	return NOP;
 }
+
+PIC16InstructionType fromOpcode(uint16_t opcode) {
+	// TODO
+	return NOP;
+}
+
+PIC16Instruction::PIC16Instruction(uint16_t opcode)
+	: type(fromOpcode(opcode)),
+	file_address(0b1111111 & opcode),
+	bit_address(0b111 & (opcode >> 7)),
+	literal(opcode), // TODO fix for CALL and GOTO instructions
+	dest_select(0b1 & (opcode >> 7))
+	{}
 
 
 
