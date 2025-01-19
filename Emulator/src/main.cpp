6b6e4773ddf8838a4b91cb8219e656a438c8fc8f
kpzip <kpzipgames@gmail.com>
2025-01-19 01:31:20 -0800

    cppify

--- Emulator/src/main.cpp
+++ Emulator/src/main.cpp
@@ -8,13 +8,12 @@
 
 int main() {
 
-	PIC16Machine *pic = malloc(sizeof(PIC16Machine));
+	PIC16Machine *pic = new PIC16Machine();
 
-	memset(pic, 0, sizeof(PIC16Machine));
 	pic->io.PORTB = 0xAA;
 	pic->io.FSR = 0x06;
 	pic->io.STATUS |= 0b10000000;
-	uint8_t *accessed = getRegFile(pic, 0x00);
+	uint8_t *accessed = pic->getRegFile(0x00);
 	printf("PORTB: %d\n", *accessed);
 	*accessed += 1;
 	printf("PORTB: %d\n", *accessed);
