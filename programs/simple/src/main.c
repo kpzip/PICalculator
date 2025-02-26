17fe666a5e3d5a8ccaba7accd9671c5a635f4221
kpzip <kpzipgames@gmail.com>
2025-02-26 10:58:52 -0800

    Update main.c

--- programs/simple/src/main.c
+++ programs/simple/src/main.c
@@ -33,13 +33,23 @@ void set_keyboard_row(uint8_t row) {
 }
 
 void display_command(uint8_t data, uint8_t rs) {
-	output_high(DSS);
+	output_low(DSS);
 	spi_write(0xF8 | (rs << 1));
 	spi_write(data & 0xF0);
 	spi_write((data << 4) & 0xF0);
-	output_low(DSS);
+	output_high(DSS);
 
 	delay_ms(10);
+}
+
+void write_last_character() {
+  if (write_pointer != 0) {
+		display_command(expr_buffer[write_pointer - 1], 1);
+	}
+		
+	if (write_pointer >= EXPR_BUF_LEN) {
+		write_pointer = 0;
+	}
 }
 
 void main() {
@@ -51,8 +61,8 @@ void main() {
 	set_tris_e(0x00);
 
 	// Chip Select pins
-	// Award winning active high chip select for DSS
-	output_low(DSS);
+	// Award winning unimplemented Chip Select!!!
+	output_high(DSS);
 	output_high(COSS);
 
 	disable_interrupts(GLOBAL);
@@ -98,21 +108,23 @@ void main() {
 			set_keyboard_row(row_counter);
 			for (column_counter = 0; column_counter < 6; column_counter++) {
 				// TODO look at the CCS compiler and figure out if having all global statics is a good idea
-				uint8_t is_on = input(KEYBOARD_IN + column_counter);
-				if (is_on) {
-					any_button_pressed = 1;
+				if (input(KEYBOARD_IN + column_counter)) {
 					uint8_t button_id = column_counter + row_counter * 6;
 					if (last_button != button_id) {
 						last_button = button_id;
+            any_button_pressed = 1;
 					} else {
-						goto TMP;
+						any_button_pressed = 2;
 					}
 				}
 			}
 		}
-		if (any_button_pressed) {
+    if (any_button_pressed == 2) {
+      // Do Nothing since we just re-registered the last button press
+    } else if (any_button_pressed == 1) {
 			uint8_t second = (status >> 1) & 1;
 			uint8_t alpha = (status >> 2) & 1;
+      // Cases are responsible for updating the display
 			switch(last_button) {
 			case 0:
 				write_pointer = 0;
@@ -130,50 +142,60 @@ void main() {
 				output_bit(RADIANS_LED, ~degrad);
 				output_bit(DEGREES_LED, degrad);
 				output_high(COSS);
-				goto TMP;
+				break;
 			////////////
 			// Numpad //
 			////////////
 			case 3:
 				// Numpad 0
 				expr_buffer[write_pointer++] = '0';
-				break;
+				write_last_character();
+        break;
 			case 8:
 				// Numpad 7
 				expr_buffer[write_pointer++] = '7';
-				break;
+				write_last_character();
+        break;
 			case 9:
 				// Numpad 8
 				expr_buffer[write_pointer++] = '8';
-				break;
+				write_last_character();
+        break;
 			case 10:
 				// Numpad 9
 				expr_buffer[write_pointer++] = '9';
-				break;
+				write_last_character();
+        break;
 			case 14:
 				// Numpad 4
 				expr_buffer[write_pointer++] = '4';
-				break;
+				write_last_character();
+        break;
 			case 15:
 				// Numpad 5
 				expr_buffer[write_pointer++] = '5';
+        write_last_character();
 				break;
 			case 16:
 				// Numpad 6
 				expr_buffer[write_pointer++] = '6';
+        write_last_character();
 				break;
 			case 20:
 				// Numpad 1
 				expr_buffer[write_pointer++] = '1';
-				break;
+				write_last_character();
+        break;
 			case 21:
 				// Numpad 2
 				expr_buffer[write_pointer++] = '2';
-				break;
+				write_last_character();
+        break;
 			case 22:
 				// Numpad 3
 				expr_buffer[write_pointer++] = '3';
-				break;
+				write_last_character();
+        break;
 			/////////////////////////
 			// 2nd, Alpha, Deg/Rad //
 			/////////////////////////
@@ -188,7 +210,7 @@ void main() {
 					output_low(DEGREES_LED);
 					output_high(RADIANS_LED);
 				}
-				goto TMP;
+				break;
 			case 24:
 				// Alpha
 				if (alpha == 0) {
@@ -198,7 +220,7 @@ void main() {
 					status &= (~(1 << 2));
 					output_low(ALPHA_LED);
 				}
-				goto TMP;
+				break;
 			case 30:
 				// 2nd
 				if (second == 0) {
@@ -208,29 +230,11 @@ void main() {
 					status &= (~(1 << 1));
 					output_low(SECOND_LED);
 				}
-				goto TMP;
+        break;
 			default:
 				// TODO other cases
-				goto TMP;
-			}
-
-			if (write_pointer != 0) {
-				display_command(expr_buffer[write_pointer - 1], 1);
-			}
-			
-			if (write_pointer >= EXPR_BUF_LEN) {
-				write_pointer = 0;
-			}
-			
-			/*
-			for (uint8_t i = 0; i < write_pointer; i++) {
-				display_command(expr_buffer[i], 1);
-			}
-			*/
-
-		TMP:	continue;
-
-
+			  break;
+      }
 		} else {
 			last_button = 255;
 		}
